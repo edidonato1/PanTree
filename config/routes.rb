27ea68c812ad71b719fb58f259d57fb53c3c8426
Eddie <edward.f.didonato@gmail.com>
2021-02-02 13:15:36 -0500

    change to password_digest

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
-  get '/auth/verify' to: 'authentication#verify'
-  
+  get '/auth/verify', to: 'authentication#verify'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :pantry

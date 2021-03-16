f8bb7837fbbf2a1db1063cd03d4285429087ab4d
Eddie <edward.f.didonato@gmail.com>
2021-03-16 11:08:49 -0400

    move uef from List to Home component

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -2,52 +2,21 @@ import { ListStyles, ListAdd, Form, MyList, Button } from './ListStyles';
 import ListItem from './ListItem';
 import React, { useContext, useEffect, useState } from 'react';
 import { createList, getOneList, addGroceryToList, addNewGroceryToList } from '../../services/lists';
-import { getAllFoods } from '../../services/foods';
-import { getAllCategories } from '../../services/categories';
-import { LoggedInUserContext } from '../../contexts/LoggedInUser';
-import { loginUser } from '../../services/auth';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBacon, faCarrot, faPrescriptionBottle, faToiletPaper } from '@fortawesome/free-solid-svg-icons';
 
 
-const List = ({ categories }) => {
-  const [loggedInUser, setLoggedInUser] = useContext(LoggedInUserContext)
-
+const List = ({ categories, foodBank, currentList, updated, setUpdated }) => {
   const [grocery, setGrocery] = useState('') // current state of input
-  const [currentList, setList] = useState([]) // most recent list associated with logged in user
-  const [foodBank, setFoods] = useState([]); // all foods in database
   const [match, setMatch] = useState(); // 
-  const [updated, setUpdated] = useState(false);
   const [isLoaded, setLoaded] = useState(false);
-
-
   const [groceryData, setGroceryData] = useState({ food_id: '' })
-
   const [foodData, setFoodData] = useState({
     name: '',
     category_id: ''
   })
 
   const icons = [faCarrot, faCarrot, faBacon, faPrescriptionBottle, faToiletPaper];
-
-  useEffect(() => {
-    const fetchFoods = async () => {
-      const data = await getAllFoods();
-      setFoods(data);
-    }
-    fetchFoods();
-
-    if (loggedInUser?.lists !== undefined) {
-      let listId = loggedInUser.lists[0].id
-      const fetchList = async () => {
-        const data = await getOneList(listId);
-        setList(data);
-      }
-      fetchList();
-    }
-
-  }, [loggedInUser, updated]);
 
   useEffect(() => {
     if (categories.length && foodBank.length) { // avoid errors while loading foods/categories
--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -1,10 +1,35 @@
 import Layout from '../../components/shared/layout/Layout';
 import List from '../../components/list/List';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { getOneList } from '../../services/lists';
 import { getAllCategories } from '../../services/categories';
+import { LoggedInUserContext } from '../../contexts/LoggedInUser';
+import { getAllFoods } from '../../services/foods';
 
 const Home = () => {
+  const [loggedInUser, setLoggedInUser] = useContext(LoggedInUserContext)
   const [categories, setCategories] = useState([])
+  const [currentList, setList] = useState([]) // most recent list associated with logged in user
+  const [foodBank, setFoods] = useState([]); // all foods in database
+  const [updated, setUpdated] = useState(false);
+
+  useEffect(() => {
+    const fetchFoods = async () => {
+      const data = await getAllFoods();
+      setFoods(data);
+    }
+    fetchFoods();
+
+    if (loggedInUser?.lists !== undefined) {
+      let listId = loggedInUser.lists[0].id
+      const fetchList = async () => {
+        const data = await getOneList(listId);
+        setList(data);
+      }
+      fetchList();
+    }
+
+  }, [loggedInUser, updated]);
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -16,7 +41,12 @@ const Home = () => {
 
   return (
     <Layout>
-      <List categories={categories} />
+      <List
+        foodBank={foodBank}
+        currentList={currentList}
+        updated={updated}
+        setUpdated={setUpdated}
+        categories={categories} />
     </Layout>
   )
 }
